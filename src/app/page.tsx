0bb91fb6f25636a1ff823f46bb5e1bd7314b7661
Ali_S <alisafarpour.1997@gmail.com>
2025-08-25 20:42:34 +0330

    refactor(page): Replace mock data with API call to fetch people data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { MOCK_DATA } from "@/constants/moc-data";
+// import { MOCK_DATA } from "@/constants/moc-data";
 import { PeopleTable } from "@/features/people-table";
 import { People } from "@/types/sw-types";
 import { Hero } from "@/features/hero";
-// import { api } from "@/services/api-sw";
+import { api } from "@/services/api-sw";
 
 export default async function Home() {
-  const tableData: People = MOCK_DATA;
+  const tableData: People = await api.getPeople();
 
   return (
     <main className="flex h-auto min-h-screen my-20 flex-col items-center justify-center bg-gradient-to-b from-background to-star-primary transition-all duration-300">
